aa84a570af30d75fbca9381574e51344b8b8cf93
Lucapar45 <69585105+Lucapar45@users.noreply.github.com>
2021-04-06 18:07:08 +0200

    updated

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ x = 50
 y = 50
 w = 30
 h = 50
-vel = 5
+vel = 10
+left = False
+right = False
+walkCounter = 0
 
+
+def redrawGameWindow():
+    global walkCounter
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (0, 0, 255), (x, y, w, h))
+    pygame.display.update()
+    
 #main loop
 run = True
 while run:
@@ -21,25 +31,24 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    #movement
-    keys = pygame.key.get_pressed()
 
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > vel:
         x-=vel
-
     if keys[pygame.K_RIGHT] and x <1600 - w - vel:
         x+=vel
-
     if keys[pygame.K_UP] and y > vel:
         y-=vel
-
     if keys[pygame.K_DOWN] and y < 900 -h - vel:
         y+=vel
-
-    win.fill((0, 0, 0))
-    #the actual character
-    pygame.draw.rect(win, (0, 0, 255), (x, y, w, h))
-    pygame.display.update()
-
+    if keys[pygame.K_a] and x > vel:
+        x-=vel
+    if keys[pygame.K_d] and x <1600 - w - vel:
+        x+=vel
+    if keys[pygame.K_w] and y > vel:
+        y-=vel
+    if keys[pygame.K_s] and y < 900 -h - vel:
+        y+=vel
+    redrawGameWindow()
 #closing the game
 pygame.quit()
